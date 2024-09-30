d9ffbb1de6f1bb2aff6a11ddfd2ac94f5eb499e9
Amanda Mbatha <amanda.mbatha@strathmore.edu>
2024-10-01 01:15:00 +0300

    lab 3 submission of a scheduled event

--- lab_submission.sql
+++ lab_submission.sql
@@ -1,1 +1,22 @@
 -- CREATE EVN_average_customer_waiting_time_every_1_hour
+
+SET GLOBAL event_scheduler = ON;
+
+
+DELIMITER $$  
+
+CREATE EVENT `EVN_average_customer_waiting_time_every_1_hour`  
+ON 
+SCHEDULE EVERY 1 HOUR  
+DO  
+BEGIN  
+    INSERT INTO customer_service_kpi (customer_service_KPI_average_waiting_time_minutes)  
+    SELECT AVG(waiting_time_minutes)  
+    FROM customer_service_ticket  
+    WHERE raised_timestamp >= NOW() - INTERVAL 1 HOUR;  
+END$$  
+
+DELIMITER ;
+
+
+SHOW EVENTS FROM `classicmodels`;
