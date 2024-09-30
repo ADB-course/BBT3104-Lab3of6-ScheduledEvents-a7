2678abb7df959718fe638fd934a711d620c094ce
Amanda Mbatha <amanda.mbatha@strathmore.edu>
2024-10-01 01:18:36 +0300

    lab 3 submission creation of an event

--- lab_submission.sql
+++ lab_submission.sql
@@ -7,13 +7,17 @@ DELIMITER $$
 
 CREATE EVENT `EVN_average_customer_waiting_time_every_1_hour`  
 ON 
-SCHEDULE EVERY 1 HOUR  
+SCHEDULE EVERY 1 HOUR   
+STARTS CURRENT_TIMESTAMP + INTERVAL 3 MINUTE  
+ON 
+COMPLETION PRESERVE  
+COMMENT 'This event computes the average waiting time of customers who raised tickets in the past hour.'  
 DO  
 BEGIN  
-    INSERT INTO customer_service_kpi (customer_service_KPI_average_waiting_time_minutes)  
-    SELECT AVG(waiting_time_minutes)  
-    FROM customer_service_ticket  
-    WHERE raised_timestamp >= NOW() - INTERVAL 1 HOUR;  
+    INSERT INTO `customer_service_kpi` (`customer_service_KPI_average_waiting_time_minutes`)   
+    SELECT AVG(TIMESTAMPDIFF(MINUTE, `customer_service_ticket_raise_time`, CURRENT_TIMESTAMP))  
+    FROM `customer_service_ticket`  
+    WHERE `customer_service_ticket_raise_time` >= NOW() - INTERVAL 1 HOUR;  
 END$$  
 
 DELIMITER ;
